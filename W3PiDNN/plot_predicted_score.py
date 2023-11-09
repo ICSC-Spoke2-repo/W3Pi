4a193c299a9244823223d5b81e83c3b0a00d2cf7
Francesco Brivio <francesco.brivio@cern.ch>
2023-11-09 16:27:35 +0100

    improve NN score plotter

--- W3PiDNN/plot_predicted_score.py
+++ W3PiDNN/plot_predicted_score.py
@@ -4,58 +4,45 @@ import matplotlib.pyplot as plt
 import os
 
 # training version
-version = 'w3pDNN_v6_signalPU200_small'
 
-# Load data
-data_s = pd.read_hdf('hdf_files/predicted/hdf_'+version+'.h5')
-data_b = pd.read_hdf('hdf_files/predicted/hdf_'+version+'_background.h5')
+train_versions = [
+    'v7', 'v8', 'v9', 'v10', 'v11', 'v11_v2', 'v12', 'v13',
+    'v14', 'v15', 'v16', 'v17', 'v18', 'v19', 'v20',
+    'v20_p1', 'v20_p2', 'v20_p3', 'v20_p4',
+]
+train_versions = [
+    'v21__35_35_25'
+]
 
-# Figure
-fig = plt.figure(figsize=(10, 7), dpi=100)
+for train_version in train_versions:
 
-# Histograms
-h_s = data_s['highest_score'].hist(alpha=0.6, grid=False, bins=100, density=True, label='signal'    , color='#3371ff')
-h_b = data_b['highest_score'].hist(alpha=0.6, grid=False, bins=100, density=True, label='background', color='#ffb833')
+    version = 'w3pDNN_'+train_version+'_signalPU200_small'
+    print("- Plotting version:", version)
 
-# Customize plot
-plt.legend()
-plt.title('Triplet highest score')
-plt.xlabel('Score')
-plt.ylabel('')
+    # Load data
+    data_s = pd.read_hdf('hdf_files/predicted/hdf_'+version+'.h5')
+    data_b = pd.read_hdf('hdf_files/predicted/hdf_'+version+'_background.h5')
 
-# Save plot
-plt.savefig('highest_score_'+version+'.png')
+    # Figure
+    fig = plt.figure(figsize=(10, 7), dpi=100)
 
-# Log y
-plt.yscale("log") 
-plt.savefig('highest_score_'+version+'_log.png')
-
-plt.close(fig)
-
-import pdb; pdb.set_trace()
-
-
-'''
-w/3 matched --> branch "genmatched"
-score > T   --> branch "highest_score"
-GenMatch    --> branch "predicted_true"
-
-
-def make_plot (variable):
-    fig = plt.figure(figsize=(10, 7), dpi=100) 
-    # Plot histograms
-    data[data['class'] == 1][variable].hist(alpha=0.6, grid=False, bins=30, label='signal'    , color='#3371ff')
-    data[data['class'] == 0][variable].hist(alpha=0.6, grid=False, bins=30, label='background', color='#ffb833')
+    # Histograms
+    h_s = data_s['highest_score'].hist(alpha=0.6, grid=False, bins=100, density=True, label='signal'    , color='#3371ff')
+    h_b = data_b['highest_score'].hist(alpha=0.6, grid=False, bins=100, density=True, label='background', color='#ffb833')
 
     # Customize plot
     plt.legend()
-    plt.title(variable)
-    plt.xlabel('Value')
+    plt.title('Triplet highest score')
+    plt.xlabel('Score')
     plt.ylabel('')
 
     # Save plot
-    plt.savefig('hdf_files/plots_'+version+'/'+variable+'.png')
+    plt.savefig('NEW_highest_score_'+version+'.png')
+
+    # Log y
+    plt.yscale("log")
+    plt.savefig('NEW_highest_score_'+version+'_log.png')
+
     plt.close(fig)
 
-
-'''
+import pdb; pdb.set_trace()
