5f03064d29837882c70c217627694ab07a4634a0
Francesco Brivio <francesco.brivio@cern.ch>
2023-12-21 17:18:31 +0100

    find pivot index in fw implementation

--- W3Pi_HLS/event_preparator/src/event_preparator.cc
+++ W3Pi_HLS/event_preparator/src/event_preparator.cc
@@ -3,49 +3,6 @@
 #include <cstdio>
 #endif
 
-Puppi BestSeed(Puppi a, bool a_masked, Puppi b, bool b_masked) {
-    bool bestByPt = (a.hwPt >= b.hwPt);
-    return (!a_masked && (bestByPt || b_masked)) ? a : b;
-    // FIXME: missing a check if both a and b are masked?
-}
-
-template<int width>
-Puppi BestSeedReduce(const Puppi x[width], const bool masked[width]) {
-    // Tree reduce from https://github.com/definelicht/hlslib/blob/master/include/hlslib/xilinx/TreeReduce.h
-    #pragma HLS inline
-
-    // Split input arrays in 2
-    static constexpr int halfWidth = width / 2;  // FIXME: how can we get rid of this division?
-    static constexpr int reducedSize = halfWidth + width % 2;
-
-    // Define reduced size arrays
-    Puppi reduced[reducedSize];
-    bool maskreduced[reducedSize];
-    #pragma HLS array_partition variable=reduced complete
-    #pragma HLS array_partition variable=maskreduced complete
-
-    // Loop to compare elements in pairs
-    for(int i = 0; i < halfWidth; ++i) {
-        #pragma HLS unroll
-        // Compare elements i and i+1, (valid for both even and odd cases) and save the best (high pt unmasked)
-        reduced[i] = BestSeed(x[i*2], masked[i*2], x[i*2+1], masked[i*2+1]);
-        maskreduced[i] = (masked[i*2] && masked[i*2+1]);
-    }
-    // if input particles are odd, the last particle hasn't been checked/compared, do it now
-    if(halfWidth != reducedSize){
-        reduced[reducedSize - 1] = x[width - 1];
-        maskreduced[reducedSize - 1] = masked[width - 1];
-    }
-    // Recursive call with reduced particles (only the ones that passes the first comparison)
-    return BestSeedReduce<reducedSize>(reduced, maskreduced);
-}
-
-template<>
-Puppi BestSeedReduce<2>(const Puppi x[2], const bool masked[2]) {
-    #pragma HLS inline
-    return BestSeed(x[0], masked[0], x[1], masked[1]);
-}
-
 template<int N>
 Puppi::pt_t SumReduce(const Puppi::pt_t in[N]) {
         // Recursive calls on first half and second half of the events
@@ -56,12 +13,6 @@ Puppi::pt_t SumReduce(const Puppi::pt_t in[N]) {
 template<>
 Puppi::pt_t SumReduce<1>(const Puppi::pt_t in[1]) {
     return in[0];
-}
-
-Puppi find_seed(const Puppi in[NPUPPI_MAX], const bool masked[NPUPPI_MAX]) {
-    #pragma HLS inline off
-	#pragma HLS pipeline ii=1
-    return BestSeedReduce<NPUPPI_MAX>(in, masked);
 }
 
 Puppi::pt_t SumReduceAll(const Puppi::pt_t in[NPUPPI_MAX]) {
@@ -91,96 +42,57 @@ inline dr2_t deltaR2(const Puppi & p1, const Puppi & p2) {
     return dphi*dphi + deta*deta;
 }
 
-void one_iteration(const Puppi in[NPUPPI_MAX], const bool masked[NPUPPI_MAX], bool masked_out[NPUPPI_MAX], Puppi & seed, Puppi::pt_t & iso_sum, dr2_t dr2_max, dr2_t dr2_veto) {
-    #pragma HLS ARRAY_PARTITION variable=in complete
-    #pragma HLS ARRAY_PARTITION variable=masked complete
-    #pragma HLS ARRAY_PARTITION variable=masked_out complete
-    #pragma HLS pipeline II=9
+bool BestSeedIdx2(Puppi a, bool a_masked, Puppi b, bool b_masked) {
+    bool bestByPt = (a.hwPt >= b.hwPt);
+    return (!a_masked && (bestByPt || b_masked)) ? true : false;
+}
 
+template<int width>
+int BestSeedReduceIdx(const Puppi x[width], const bool masked[width], const int my_indexes[width]) {
+    // Tree reduce from https://github.com/definelicht/hlslib/blob/master/include/hlslib/xilinx/TreeReduce.h
+    #pragma HLS inline
+
+    // Compute half width of input arrays
+    static constexpr int halfWidth = width / 2;
+    static constexpr int reducedSize = halfWidth + width % 2;
+
+    // Define reduced size arrays
+    Puppi reduced[reducedSize];
+    bool maskreduced[reducedSize];
+    int indexesreduced[reducedSize];
+    #pragma HLS array_partition variable=reduced complete
+    #pragma HLS array_partition variable=maskreduced complete
+    #pragma HLS array_partition variable=indexesreduced complete
+
+    // Loop to compare elements in pairs
+    for(int i = 0; i < halfWidth; ++i) {
+        #pragma HLS unroll
+        bool first_best = BestSeedIdx2(x[i*2], masked[i*2], x[i*2+1], masked[i*2+1]);
+        reduced[i] = first_best ? x[i*2] : x[i*2+1];
+        indexesreduced[i] = first_best ? my_indexes[i*2]: my_indexes[i*2+1];
+        maskreduced[i] = (masked[i*2] && masked[i*2+1]);
+    }
+    // if input particles are odd, the last particle hasn't been checked/compared, do it now
+    if(halfWidth != reducedSize){
+        reduced[reducedSize - 1] = x[width - 1];
+        indexesreduced[reducedSize - 1] = my_indexes[width - 1];
+        maskreduced[reducedSize - 1] = masked[width - 1];
+    }
+    // Recursive call with reduced particles (only the ones that passes the first comparison)
+    return BestSeedReduceIdx<reducedSize>(reduced, maskreduced, indexesreduced);
+}
+
+template<>
+int BestSeedReduceIdx<2>(const Puppi x[2], const bool masked[2], const int my_indexes[2]) {
+    #pragma HLS inline
+    return BestSeedIdx2(x[0], masked[0], x[1], masked[1]) ? my_indexes[0] : my_indexes[1];
+}
+
+int find_pivot_idx(const Puppi in[NPUPPI_MAX], const bool masked[NPUPPI_MAX], const int my_indexes[NPUPPI_MAX]) {
     #pragma HLS inline off
-
-    // Define list of pts to sum (particle inside isolation cone)
-    Puppi::pt_t tosum[NPUPPI_MAX];
-    #pragma HLS ARRAY_PARTITION variable=tosum complete
-
-    // Find seed (charged part with highest pt)
-    seed = find_seed(in, masked);
-
-    // Loop on all particles to compute iso of seed
-    // FIXME: avoid the comparison with the seed itself? (maybe using seed index...)
-    for (unsigned int i = 0; i < NPUPPI_MAX; ++i) {
-        // Get dR2
-        dr2_t dr2 = deltaR2(seed, in[i]);
-        // Check if paricle is inside isolation cone
-        bool inside = (dr2 < dr2_max);
-        // if inside (used for this seed) then mask it for next iterations
-        masked_out[i] = masked[i] || inside;
-        // if inside and not in veto cone, get pt for iso computation
-        tosum[i] =  inside && (dr2 > dr2_veto) ? in[i].hwPt : Puppi::pt_t(0);
-    }
-    iso_sum = SumReduceAll(tosum);
+	#pragma HLS pipeline ii=1
+    return BestSeedReduceIdx<NPUPPI_MAX>(in, masked, my_indexes);
 }
-
-void compute_isolated_w3p(const Puppi in[NPUPPI_MAX], Puppi out[NISO_MAX], Puppi::pt_t out_absiso[NISO_MAX])  {
-    #pragma HLS ARRAY_PARTITION variable=in complete
-    #pragma HLS ARRAY_PARTITION variable=out complete
-    #pragma HLS ARRAY_PARTITION variable=out_absiso complete
-    #pragma HLS pipeline II=9
-
-    // Define min/max isolation cones
-    const dr2_t dr2_max = drToHwDr2(0.4), dr2_veto = drToHwDr2(0.1);
-
-    // Define masked lists
-    bool masked[NPUPPI_MAX], masked_out[NPUPPI_MAX];
-    #pragma HLS ARRAY_PARTITION variable=masked complete
-    #pragma HLS ARRAY_PARTITION variable=masked_out complete
-
-    // Define
-    Puppi::pt_t tosum[NPUPPI_MAX];
-    #pragma HLS ARRAY_PARTITION variable=tosum complete
-
-    // Mask neutral particles
-    for (unsigned int i = 0; i < NPUPPI_MAX; ++i) {
-        masked[i] = in[i].hwID <= 1;
-    }
-
-    // Clear output arrays
-    for (unsigned int j = 0; j < NISO_MAX; ++j) {
-        out[j].clear();
-        out_absiso[j] = 0;
-    }
-
-    int niso = 0;
-    for (unsigned int j = 0; j < NISO_MAX; ++j) {
-        // Define seed (candidate output isolated particle) and its iso
-        Puppi seed;
-        Puppi::pt_t iso_sum;
-
-        // Do actual processing here for this seed
-        one_iteration(in, masked, masked_out, seed, iso_sum, dr2_max, dr2_veto);
-
-        // Mask also vetoed particles
-        for (unsigned int i = 0; i < NPUPPI_MAX; ++i) {
-            masked[i] = masked_out[i];
-        }
-#ifndef __SYNTHESIS__
-        // Debug printout
-        //printf("HW Seed pT %8.3f eta %+6.3f phi %+6.3f pid %1u: abs iso %8.3f\n",
-        //    seed.floatPt(), seed.floatEta(), seed.floatPhi(), seed.hwID.to_uint(), Puppi::floatPt(iso_sum));
-#endif
-        // If seed is isolated, store it in the output
-        if (iso_sum < seed.hwPt) {
-            out[niso] = seed;
-            out_absiso[niso] = iso_sum;
-            niso++;
-        }
-    }
-
-}
-
-// ------------------------------------------------------------------------------------------------------------------------------
-//      ***  NEW SECTION  ***
-// ------------------------------------------------------------------------------------------------------------------------------
 
 // Apply selections to filter only good seed-candidates:
 //  - pdgID = +/- 211 || +/- 11
@@ -301,13 +213,20 @@ void event_preparator (const Puppi input[NPUPPI_MAX], Puppi & pivot)
     // Debug printout
     for (unsigned int i = 0; i < 45; i++)
         if (!masked[i])
-            std::cout << " -> Seed " << i << " pT: " << input[i].hwPt << " eta: " << input[i].hwEta*Puppi::ETAPHI_LSB << " ID: " << input[i].hwID
+            std::cout << " -> Seed " << i << " pT: " << input[i].hwPt << " eta: " << input[i].hwEta*Puppi::ETAPHI_LSB << " pdgID: " << input[i].hwID
                     << " charge: " << input[i].charge() << " IsoSum: " << output_absiso[i] << " Masked: " << masked[i] << std::endl;
 
+    // Indexes array
+    int my_indexes[NPUPPI_MAX];
+    #pragma HLS ARRAY_PARTITION variable=my_indexes complete
+    for (unsigned int i=0; i < NPUPPI_MAX; i++)
+        my_indexes[i] = i;
+
     // Find pivot (charged filtered candidate with highest pt)
-    //Puppi pivot = find_seed(input, masked);
-    pivot = find_seed(input, masked); // FIXME: use line above when implementing next part (remove pivot from arguments)
+    int pivot_idx = find_pivot_idx(input, masked, my_indexes);
+    pivot = input[pivot_idx];
 
     // Debug printout
-    std::cout << "---> Pivot    pT: " << pivot.hwPt << " eta: " << pivot.hwEta*Puppi::ETAPHI_LSB << " ID: " << pivot.hwID << std::endl;
+    std::cout << "---> Pivot     idx: " << pivot_idx << std::endl;
+    std::cout << "     Pivot     pT : " << pivot.hwPt << " eta: " << pivot.hwEta*Puppi::ETAPHI_LSB << " pdgID: " << pivot.hwID << std::endl;
 }
